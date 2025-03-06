07189d96044f2685e1d374588783619a43c231bd
tomast1337 <vycasnicolas@gmail.com>
2025-03-06 12:23:19 -0300

    Implement file reading and parsing in main function; iterate and print notes

--- src/main.zig
+++ src/main.zig
@@ -4,13 +4,35 @@ const nbs = @import("lib/nbs.zig");
 
 // Example usage
 pub fn main() !void {
-    std.io.getStdOut().write("Reading file...\n", .{});
-    defer std.io.getStdOut().write("Done.\n", .{});
-}
+    const stdout = std.io.getStdOut().writer();
+    const allocator = std.heap.page_allocator;
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    // Open the file
+    const path = "nyan_cat.nbs";
+    const file = try fs.cwd().openFile(path, .{});
+    defer file.close();
+
+    // Get file size
+    const stat = try file.stat();
+    const buffer = try allocator.alloc(u8, stat.size);
+    defer allocator.free(buffer);
+
+    // Read the file into the buffer
+    const size = try file.readAll(buffer);
+    if (size != stat.size) {
+        try stdout.print("Failed to read the file\n", .{});
+    }
+
+    // Parse the content
+    var nbsParser = nbs.NBSParser.init(buffer);
+
+    // Print the parsed content
+    var nbsFile = try nbsParser.parse(allocator);
+    defer nbsFile.deinit();
+    try stdout.print("{}\n", .{nbsFile.header});
+
+    // Iterate over notes and print them
+    for (nbsFile.notes.items) |note| {
+        try stdout.print("note: {d} {d} {d} {d} {d} {d} {d}\n", .{ note.tick, note.layer, note.instrument, note.key, note.velocity, note.panning, note.pitch });
+    }
 }
